24d463be5b0965b49800901417639925dfc0507c
I'amjk <goldm47@gmail.com>
2020-03-06 00:48:50 +0100

    Can get status from start

--- run.py
+++ run.py
@@ -8,4 +8,8 @@ from reverse.bot import Bot
 
 # Create instance of Bot implementing Reverse
 _reverse = Bot(description="The Reverse", command_prefix="!", pm_help = False)
-_reverse.run(sys.argv[1])
+
+try:
+    _reverse.run(sys.argv[1], sys.argv[2])
+except:
+    _reverse.run(sys.argv[1])
