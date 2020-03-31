905052da9c87378908a7ab5c939b7efcedd29292
I'amjk <goldm47@gmail.com>
2020-03-31 20:39:03 +0200

    Ajout d'une fonction qui permet au cogs d'appeler leur propre fichier de configuration

--- reverse/core/utils.py
+++ reverse/core/utils.py
@@ -12,6 +12,14 @@ def _load_config(name):
     with utils_open(jsonf) as fp:
         return json.load(fp)
 
+def load_custom_config(name, file, path = '_env'):
+    """Open file and return a corresponding JSON object.
+    """
+    cwfd = os.path.dirname(os.path.realpath(file))  # the directory of utils.py
+    jsonf = os.path.realpath(os.path.join(cwfd, path, name))
+    with utils_open(jsonf) as fp:
+        return json.load(fp)
+
 def _create_folder(folder):
     import pathlib
     cwfd = os.path.dirname(os.path.realpath(__file__))  # the directory of utils.py
