ffce187fa78a8f2236768f3e15ec07f51a31966e
Incisif <incisiflefufu@gmail.com>
2021-01-10 18:36:18 +0100

    Ajout de nouvelle fonctionnalité de mordération, countfrom et delete

--- reverse/client/default.py
+++ reverse/client/default.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import asyncio
 from urllib import parse
+from reverse.core import utils
 
 
 class DefaultCog(commands.Cog):
@@ -20,15 +21,76 @@ class DefaultCog(commands.Cog):
 		author  = ctx.author
 		guild   = ctx.guild
 
-		if(await self.specifiedRole("Cleaner", guild, author, ctx=ctx)):
+		if(await utils.specifiedRole("Cleaner", guild, author, ctx=ctx)):
 			if(wait > 0):
 				await ctx.send("Removed in {}".format(wait))
 				await asyncio.sleep(wait)
 			async for message in channel.history(limit=amount+1):
 				await message.delete()
+	
+	@commands.command(pass_context=True, aliases=['mc', 'dcf'])
+	async def countfrom(self, ctx, *args):
+		"""Count the number of messages from link. Aliases: !mc !dcf"""
+		if(not args):
+			await ctx.send("Specify url")
+			raise Exception("Cancelled")
+
+		channel = ctx.message.channel
+		author  = ctx.author
+		guild   = ctx.guild
+		msg = None
+
+		checker = ["channels", str(guild.id)]
+		for channel in guild.text_channels:
+			checker.append(str(channel.id))
+
+		for url in args:
+			parsed_url = parse.urlparse(url)
+			path_message = parsed_url.path.split("/")[1:]
+			if(utils.isListContains(checker, path_message[:3])):
+				channel = guild.get_channel(int(path_message[2]))
+				try:
+					msg = await channel.fetch_message(int(path_message[3]))
+				except:
+					print("could find message")
+					return
+
+		channel = channel or ctx.channel
+		count = 0
+
+		#TODO : Verifier que le bot a acces au channel avant de compter
+		async for message in channel.history(limit=None):
+			count += 1
+			if(message == msg):
+				break
+		
+		#TODO : Choisir un meilleur message
+		await ctx.send("Message is {}+1 layers deep.".format(count))
+	
+				if(self.isListContains(checker, path_message[:3])):
+					channel = guild.get_channel(int(path_message[2]))
+					try:
+						msg = await channel.fetch_message(int(path_message[3]))
+
+	async def get_message(self, guild, url):
+		checker = ["channels", str(guild.id)]
+		for channel in guild.text_channels:
+			checker.append(str(channel.id))
+
+		parsed_url = parse.urlparse(url)
+		path_message = parsed_url.path.split("/")[1:]
+		if(utils.isListContains(checker, path_message[:3])):
+			channel = guild.get_channel(int(path_message[2]))
+			try:
+				msg = await channel.fetch_message(int(path_message[3]))
+				return msg
+			except:
+				print("could find message")
+				return None
 
 	@commands.command(pass_context = True)
 	async def delete(self, ctx, *args):
+		"""Delete specific message from link."""
 		if(not args):
 			await ctx.send("Specify url")
 			raise Exception("Cancelled")
@@ -37,7 +99,7 @@ class DefaultCog(commands.Cog):
 		author  = ctx.author
 		guild   = ctx.guild
 
-		if(await self.specifiedRole("Cleaner", guild, author, ctx=ctx)):
+		if(await utils.specifiedRole("Cleaner", guild, author, ctx=ctx)):
 			checker = ["channels", str(guild.id)]
 			for channel in guild.text_channels:
 				checker.append(str(channel.id))
@@ -45,41 +107,13 @@ class DefaultCog(commands.Cog):
 			for url in args:
 				parsed_url = parse.urlparse(url)
 				path_message = parsed_url.path.split("/")[1:]
-				if(self.isListContains(checker, path_message[:3])):
+				if(utils.isListContains(checker, path_message[:3])):
 					channel = guild.get_channel(int(path_message[2]))
 					try:
 						msg = await channel.fetch_message(int(path_message[3]))
-						await msg.delete()
+						print(msg)
 					except:
 						print("could find message")
-		
-	async def specifiedRole(self, name: str, guild: list, author: list, attr: str = "name", ctx = None):
-		g_role = False
-		a_role = False
-		if(g_role := self.isNameInList(name, guild.roles)) == False:
-			if(ctx is not None):
-				await ctx.send("You need to create the role `{}` to use this on you server.".format(name))
-		if(a_role := self.isNameInList(name, author.roles)) == False:
-			if(ctx is not None):
-				await ctx.send("You need the role `{}`.".format(name))
-		return all([g_role, a_role])
-
-	def isListContains(self, lesser: list, bigger: list):
-		"""Check if bigger contains all elements in lesser"""
-		return all(elem in lesser for elem in bigger)
-
-	def isNameInList(self, name: str, array: list, attr: str = "name"):
-		"""Look for string in list of object on specific attribute
-
-		Keyword arguments:
-		name 	-- String to find
-		array 	-- List of Object
-		attr 	-- Attribute to check in the Object
-		"""
-		for role in array:
-			if(name == getattr(role, attr)):
-				return True
-		return False
 
 def setup(bot):
 	bot.add_cog(DefaultCog(bot))
