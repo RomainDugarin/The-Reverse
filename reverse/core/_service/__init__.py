ea305ce387418f3324eea70375e2bbcc24e7c536
Incisif <incisiflefufu@gmail.com>
2021-01-10 18:42:36 +0100

    Petit modification lié au core

--- reverse/core/_service/__init__.py
+++ reverse/core/_service/__init__.py
@@ -3,3 +3,4 @@ __import__('pkg_resources').declare_namespace(__name__)
 from .sqlite import SqliteService
 from .betaseries import BetaSeries, Route, json_or_text
 from .task import TaskService, loop
+from .logger import ReverseLogger
