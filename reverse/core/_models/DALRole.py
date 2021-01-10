ff1d2dbd0a08ac23cc15c1c544d4bc31a9e454a4
Incisif <incisiflefufu@gmail.com>
2021-01-10 18:42:59 +0100

    Ajout d'un modèle pour les updates future

---
+++ reverse/core/_models/DALRole.py
@@ -0,0 +1,36 @@
+from discord import Colour
+
+class DALRole():
+
+	__slots__=[
+		'name',
+		'permission',
+		'colour',
+		'hoist',
+		'mentionable',
+		'reason',
+		'regex',
+		'position',
+		'uniqueness',
+		'magicmultiplier',
+		'oid',
+		'supplies'
+	]
+	
+	def __init__(self,
+		name='new role',
+		permissions=None,
+		colour=0xffffff,
+		hoist=False,
+		mentionable=False,
+		reason="Create for the Drug Addict Lair Game of Kings.",
+		regex="",
+		position=0,
+		uniqueness=0,
+		magicmultiplier=1.2,
+		oid=None,
+		supplies=0):
+        self.data = message
+        self.run()
+
+	
