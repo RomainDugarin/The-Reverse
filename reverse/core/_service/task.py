f9d4f8eb61f8dae0d41a71d5e60a016b76610dd9
Incisif <incisiflefufu@gmail.com>
2021-01-10 18:43:53 +0100

    Permet au service de mettre à jour les task avec de nouveau interval Ajout de Change_interval et recalculate_interval

--- reverse/core/_service/task.py
+++ reverse/core/_service/task.py
@@ -36,9 +36,18 @@ class TaskService:
 		self.fields.append(loop)
 		return loop
 
+	def change_interval(self, loop, *, seconds=0, minutes=0, hours=0):
+		loop.change_interval(seconds=seconds, minutes=minutes, hours=hours)
+
 	def sleep_until(self, when, func):
 		delta = utils.time_until(when)
 		return self.createLoop(func, seconds=delta)
+
+	def recalculate_interval(self, loop: str, when: datetime):
+		delta = utils.time_until(when)
+		loop = self.findTaskByName(loop)
+		if(loop):
+			loop.change_interval(seconds=int(delta))
 		
 	def createLoop(self, func, *, seconds=0, minutes=0, hours=0, count=None, reconnect=True, loop=None, ctx=None, data=None):
 		return Loop(func, seconds=seconds, minutes=minutes, hours=hours, count=count, reconnect=reconnect, loop=loop, ctx=ctx, data=data)
