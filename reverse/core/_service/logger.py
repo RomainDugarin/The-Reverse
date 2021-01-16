91f21765adde4fe4bded0a17711522b67cfb4de0
Incisif <incisiflefufu@gmail.com>
2021-01-17 00:23:22 +0100

    Better reverse logger

--- reverse/core/_service/logger.py
+++ reverse/core/_service/logger.py
@@ -11,19 +11,31 @@ class ReverseLogger(logging.Logger):
 		'handler'
         'env'
     ]
-	
-	def __init__(self, name, path="./", logformat=""):
-		self.name = name
-		self.instance = logging.getLogger(name)
+
+	def __init__(self, name: str, level=logging.DEBUG, path="./", logformat="", initLog: bool = False) -> None:
+		"""Create a logger
+
+		Parameters
+		----------
+		name : str
+			Logger name
+		level : [type], optional
+			Sets the threshold for this logger. Logging messages which are less severe than level will be ignored, by default logging.DEBUG
+		path : str, optional
+			Logger filepath, by default "./"
+		logformat : str, optional
+			Specify the layout of log records in the final output, by default ""
+		initLog : bool, optional
+			Send a record at initialization, by default False
+		"""
+		super().__init__(name, level=level)
 		self.logfile = "{}{}.log".format(path,name)
 		self.logformat = logformat or ("[%(asctime)s] %(levelname)-8s :: %(message)s")
 		self.formatter = logging.Formatter(self.logformat)
 		self.handler = logging.FileHandler(self.logfile, encoding="utf-8")
 
 		self.handler.setFormatter(self.formatter)
-		self.instance.addHandler(self.handler)
-		self.instance.setLevel(logging.DEBUG)
-		self.getInstance().info("Creation of the logging instance - {}/{}".format(self.name, self.logfile))
-
-	def getInstance(self) -> logging.Logger : 
-		return self.instance
+		self.addHandler(self.handler)
+		self.setLevel(logging.DEBUG)
+		# Message of initialization 
+		if(initLog): self.info("Creation of the logging instance - {}/{}".format(self.name, self.logfile))
